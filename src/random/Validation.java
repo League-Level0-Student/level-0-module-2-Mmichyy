42fa36adda5d6df5e9eca6f91bc3e8e6238401f7
CatcusCat <wjmorris2004@gmail.com>
2017-11-25 10:56:18 -0800

    rock paper scissors,dice roll,validation

--- src/random/Validation.java
+++ src/random/Validation.java
@@ -19,7 +19,11 @@ public class Validation {
 		System.out.println(randomNumber);
 
 		// 3. Use each value of randomNumber to give the user a random compliment.
+		if (3) {
+			
+		} else {
 
+		}
 		// 4. Repeat all the code above 10 times
 		
 		// 5. Find someone to test out your program. They will like it :)
